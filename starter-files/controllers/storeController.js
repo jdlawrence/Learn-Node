b5d3213f28a17b5e4655ac19458ebec7d5edcdf0
Jamil Lawrence <jamil.lawrence@gmail.com>
2017-09-26 22:07:43 -0700

    14 in progress

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -10,7 +10,18 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores});
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({_id: req.params.id});
+  res.render('editStore', { title: `Edit ${store.name}`, store });  
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { homePage, addStore, createStore } = require('../controllers/storeController');
+const { homePage, addStore, createStore, getStores, editStore } = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', homePage);
+router.get('/', catchErrors(getStores));
+router.get('/stores', catchErrors(getStores));
 
 router.get('/add', addStore);
 
 router.post('/add', catchErrors(createStore));
+
+router.get('/stores/:id/edit', catchErrors(editStore));
+
 
 module.exports = router;
