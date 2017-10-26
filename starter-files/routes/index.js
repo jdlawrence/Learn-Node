629142775b50b7a2b1122a79d1fdc4611f2f85ca
Jamil Lawrence <jamil.lawrence@gmail.com>
2017-10-25 19:35:46 -0700

    #34 in progress

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -48,6 +48,7 @@ router.post('/account/reset/:token',
   authController.confirmedPasswords, 
   catchErrors(authController.update)  
 );
+router.get('/map', storeController.mapPage);
 
 //************ API ROUTES */
 router.get('/api/search/', catchErrors(storeController.searchStores)); 
